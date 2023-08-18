10c46efb0c7e9e58acaf6c6da70f543e2aa12123
ariel.pineiro <ariel.pineiro@iskaypet.com>
2023-08-18 11:01:02 -0300

    fix

--- env/config.go
+++ env/config.go
@@ -18,8 +18,8 @@ type Config struct {
 // SCOPE LOCAL (local) or REMOTE (remote)
 // ENV DEV|UAT|REMOTE.
 func Load(config ...Config) error {
-	environment := GetEnv()
-	scope := GetScope()
+	currentEnv := GetEnv()
+	currentScope := GetScope()
 	settings := &Config{
 		Filename: "config.yaml",
 		Folder:   "config",
@@ -35,7 +35,7 @@ func Load(config ...Config) error {
 	}
 
 	for {
-		if isProjectRoot(root) {
+		if found(filepath.Join(root, "go.mod")) {
 			break
 		}
 		root = filepath.Dir(root)
@@ -44,13 +44,13 @@ func Load(config ...Config) error {
 	propertiesPath := fmt.Sprintf("%s/%s", root, settings.Folder)
 	var compositeConfig []string
 
-	envConfig := fmt.Sprintf("%s/%s/%s.%s", propertiesPath, scope, environment, settings.Filename)
+	envConfig := fmt.Sprintf("%s/%s/%s.%s", propertiesPath, currentScope, currentEnv, settings.Filename)
 	if found(envConfig) {
 		slog.Info(fmt.Sprintf("go-config: append %s ...", envConfig))
 		compositeConfig = append(compositeConfig, envConfig)
 	}
 
-	scopeConfig := fmt.Sprintf("%s/%s/%s", propertiesPath, scope, settings.Filename)
+	scopeConfig := fmt.Sprintf("%s/%s/%s", propertiesPath, currentScope, settings.Filename)
 	if found(scopeConfig) {
 		slog.Info(fmt.Sprintf("go-config: append %s ...", scopeConfig))
 		compositeConfig = append(compositeConfig, scopeConfig)
@@ -67,16 +67,17 @@ func Load(config ...Config) error {
 		return err
 	}
 
-	slog.Info(fmt.Sprintf("ENV: %s, SCOPE: %s", environment, scope))
+	slog.Info(fmt.Sprintf("ENV: %s, SCOPE: %s", currentEnv, currentScope))
 
 	return nil
 }
 
-func isProjectRoot(dir string) bool {
-	return found(filepath.Join(dir, "go.mod"))
-}
-
 func found(path string) bool {
-	_, err := os.Stat(path) // ignore
-	return err == nil
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		slog.Error(fmt.Sprintf("go-config: %s", err))
+		return false
+	}
+	slog.Debug(fmt.Sprintf("go-config: path %s, fileInfo: %s", path, fileInfo.Name()))
+	return true
 }
