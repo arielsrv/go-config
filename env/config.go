b66f46d9aecdbb0a042e2a80f09cf02ba82a8123
ariel.pineiro <ariel.pineiro@iskaypet.com>
2023-08-20 18:27:42 -0300

    refactor

--- env/config.go
+++ env/config.go
@@ -1,18 +1,20 @@
 package env
 
 import (
-	"fmt"
 	"log/slog"
 	"os"
-	"path/filepath"
-
-	"github.com/joho/godotenv"
 )
 
 type Config struct {
 	File   string
 	Path   string
 	Logger *slog.Logger
+}
+
+var config *Config
+
+func init() {
+	config = New()
 }
 
 func New() *Config {
@@ -26,21 +28,15 @@ func New() *Config {
 	return c
 }
 
-var config *Config
-
-func init() {
-	config = New()
-}
-
-func SetConfigPath(folder string) {
-	if !IsEmptyString(folder) {
-		config.Path = folder
+func SetConfigPath(configPath string) {
+	if !IsEmptyString(configPath) {
+		config.Path = configPath
 	}
 }
 
-func SetConfigFile(filename string) {
-	if !IsEmptyString(filename) {
-		config.File = filename
+func SetConfigFile(configFile string) {
+	if !IsEmptyString(configFile) {
+		config.File = configFile
 	}
 }
 
@@ -48,69 +44,4 @@ func SetLogger(logger *slog.Logger) {
 	if logger != nil {
 		config.Logger = logger
 	}
-}
-
-func Reset() {
-	os.Clearenv()
-	config = New()
-}
-
-// Load
-// SCOPE LOCAL (local) or REMOTE (remote)
-// ENV DEV|UAT|REMOTE.
-func Load() error {
-	root, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-
-	for {
-		if PathExists(filepath.Join(root, "go.mod")) {
-			break
-		}
-		root = filepath.Dir(root)
-	}
-
-	propertiesPath := fmt.Sprintf("%s/%s", root, config.Path)
-	var compositeConfig []string
-
-	env := GetEnv()
-	scope := GetScope()
-
-	envConfig := fmt.Sprintf("%s/%s/%s.%s", propertiesPath, scope, env, config.File)
-	if PathExists(envConfig) {
-		config.Logger.Info(fmt.Sprintf("go-config: append %s ...", envConfig))
-		compositeConfig = append(compositeConfig, envConfig)
-	}
-
-	scopeConfig := fmt.Sprintf("%s/%s/%s", propertiesPath, scope, config.File)
-	if PathExists(scopeConfig) {
-		config.Logger.Info(fmt.Sprintf("go-config: append %s ...", scopeConfig))
-		compositeConfig = append(compositeConfig, scopeConfig)
-	}
-
-	sharedConfig := fmt.Sprintf("%s/%s", propertiesPath, config.File)
-	if PathExists(fmt.Sprintf("%s/%s", propertiesPath, config.File)) {
-		config.Logger.Info(fmt.Sprintf("go-config: append %s ...", sharedConfig))
-		compositeConfig = append(compositeConfig, sharedConfig)
-	}
-
-	err = godotenv.Load(compositeConfig...)
-	if err != nil {
-		return err
-	}
-
-	config.Logger.Info(fmt.Sprintf("ENV: %s, SCOPE: %s", env, scope))
-
-	return nil
-}
-
-func PathExists(path string) bool {
-	fileInfo, err := os.Stat(path)
-	if err != nil {
-		config.Logger.Error(fmt.Sprintf("go-config: %s", err))
-		return false
-	}
-	config.Logger.Debug(fmt.Sprintf("go-config: path %s, fileInfo: %s", path, fileInfo.Name()))
-	return true
 }
---
+++ env/loader.go
@@ -0,0 +1,74 @@
+package env
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
+)
+
+func Reset() {
+	os.Clearenv()
+	config = New()
+}
+
+// Load
+// SCOPE LOCAL (local) or REMOTE (remote)
+// ENV DEV|UAT|REMOTE.
+func Load() error {
+	root, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	for {
+		if PathExists(filepath.Join(root, "go.mod")) {
+			break
+		}
+		root = filepath.Dir(root)
+	}
+
+	propertiesPath := fmt.Sprintf("%s/%s", root, config.Path)
+	var compositeConfig []string
+
+	env := GetEnv()
+	scope := GetScope()
+
+	envConfig := fmt.Sprintf("%s/%s/%s.%s", propertiesPath, scope, env, config.File)
+	if PathExists(envConfig) {
+		config.Logger.Info(fmt.Sprintf("go-config: append %s ...", envConfig))
+		compositeConfig = append(compositeConfig, envConfig)
+	}
+
+	scopeConfig := fmt.Sprintf("%s/%s/%s", propertiesPath, scope, config.File)
+	if PathExists(scopeConfig) {
+		config.Logger.Info(fmt.Sprintf("go-config: append %s ...", scopeConfig))
+		compositeConfig = append(compositeConfig, scopeConfig)
+	}
+
+	sharedConfig := fmt.Sprintf("%s/%s", propertiesPath, config.File)
+	if PathExists(fmt.Sprintf("%s/%s", propertiesPath, config.File)) {
+		config.Logger.Info(fmt.Sprintf("go-config: append %s ...", sharedConfig))
+		compositeConfig = append(compositeConfig, sharedConfig)
+	}
+
+	err = godotenv.Load(compositeConfig...)
+	if err != nil {
+		return err
+	}
+
+	config.Logger.Info(fmt.Sprintf("ENV: %s, SCOPE: %s", env, scope))
+
+	return nil
+}
+
+func PathExists(path string) bool {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		config.Logger.Error(fmt.Sprintf("go-config: %s", err))
+		return false
+	}
+	config.Logger.Debug(fmt.Sprintf("go-config: path %s, fileInfo: %s", path, fileInfo.Name()))
+	return true
+}
