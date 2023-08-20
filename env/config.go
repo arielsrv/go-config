8cf3384468de7b33e412bc785f4a15a5d5fc5a4c
ariel.pineiro <ariel.pineiro@iskaypet.com>
2023-08-20 11:28:52 -0300

    rename

--- env/config.go
+++ env/config.go
@@ -58,26 +58,26 @@ func Load() error {
 
 	root := findRoot(wd)
 
-	propertiesPath := fmt.Sprintf("%s/%s", root, config.Folder)
+	configPath := fmt.Sprintf("%s/%s", root, config.Folder)
 	var compositeConfig []string
 
 	env := GetEnv()
 	scope := GetScope()
 
-	envConfig := fmt.Sprintf("%s/%s/%s.%s", propertiesPath, scope, env, config.Filename)
+	envConfig := fmt.Sprintf("%s/%s/%s.%s", configPath, scope, env, config.Filename)
 	if PathExists(envConfig) {
 		slog.Info(fmt.Sprintf("go-config: append %s ...", envConfig))
 		compositeConfig = append(compositeConfig, envConfig)
 	}
 
-	scopeConfig := fmt.Sprintf("%s/%s/%s", propertiesPath, scope, config.Filename)
+	scopeConfig := fmt.Sprintf("%s/%s/%s", configPath, scope, config.Filename)
 	if PathExists(scopeConfig) {
 		slog.Info(fmt.Sprintf("go-config: append %s ...", scopeConfig))
 		compositeConfig = append(compositeConfig, scopeConfig)
 	}
 
-	sharedConfig := fmt.Sprintf("%s/%s", propertiesPath, config.Filename)
-	if PathExists(fmt.Sprintf("%s/%s", propertiesPath, config.Filename)) {
+	sharedConfig := fmt.Sprintf("%s/%s", configPath, config.Filename)
+	if PathExists(fmt.Sprintf("%s/%s", configPath, config.Filename)) {
 		slog.Info(fmt.Sprintf("go-config: append %s ...", sharedConfig))
 		compositeConfig = append(compositeConfig, sharedConfig)
 	}
