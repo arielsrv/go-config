04605e9cd9c5b202161dd58de490b1eedc40d10f
ariel.pineiro <ariel.pineiro@iskaypet.com>
2023-09-14 13:56:10 +0200

    doc

--- doc.go
+++ doc.go
@@ -1,3 +1,11 @@
+// Package config
+// go-config is a configuration library
+// for accessing a mixture of static as well as dynamic configurations as a single configuration unit.
+// There are two key concepts to note:
+//
+// - Properties that can be read by your code.
+// - Configurations that organize properties into objects you can bootstrap your application with.
+
 package config
 
 import (
